ea45574b9c9ca579d814351a03540fca7d43e482
Gerben <gerben@treora.com>
2016-03-05 19:08:32 +0100

    Handle Ranges with non-TextNode containers as well

--- highlight-range.js
+++ highlight-range.js
@@ -58,11 +58,8 @@ function highlightRange(rangeObject, highlightClass) {
 
 // Return an array of the text nodes in the range. Split the start and end nodes if required.
 function textNodesInRange(rangeObject) {
-    // Assert that the start and end nodes are text nodes.
-    if (rangeObject.startContainer.nodeType != Node.TEXT_NODE
-        || rangeObject.endContainer.nodeType != Node.TEXT_NODE) {
-        throw new Error("Range end points should be text nodes. Support for other types is not implemented.");
-    }
+    // Modify Range to make sure that the start and end nodes are text nodes.
+    setRangeToTextNodes(rangeObject);
 
     var nodes = [];
 
@@ -122,6 +119,52 @@ function textNodesInRange(rangeObject) {
     }
 
     return nodes;
+}
+
+
+// Normalise the range to start and end in a text node.
+// Copyright (c) 2015 Randall Leeds
+function setRangeToTextNodes(rangeObject) {
+    function getFirstTextNode(node) {
+        if (node.nodeType === Node.TEXT_NODE) return node;
+        var document = node.ownerDocument;
+        var walker = document.createTreeWalker(node, NodeFilter.SHOW_TEXT, null, false);
+        return walker.firstChild();
+    }
+
+    var startNode = rangeObject.startContainer;
+    var startOffset = rangeObject.startOffset;
+
+    // Drill down to a text node if the range starts at the container boundary.
+    if (startNode.nodeType !== Node.TEXT_NODE) {
+        if (startOffset === startNode.childNodes.length) {
+            startNode = startNode.childNodes[startOffset - 1];
+            startNode = getFirstTextNode(startNode);
+            startOffset = startNode.textContent.length;
+        } else {
+            startNode = startNode.childNodes[startOffset];
+            startNode = getFirstTextNode(startNode);
+            startOffset = 0;
+        }
+        rangeObject.setStart(startNode, startOffset);
+    }
+
+    var endNode = rangeObject.endContainer;
+    var endOffset = rangeObject.endOffset;
+
+    // Drill down to a text node if the range ends at the container boundary.
+    if (endNode.nodeType !== Node.TEXT_NODE) {
+        if (endOffset === endNode.childNodes.length) {
+            endNode = endNode.childNodes[endOffset - 1];
+            endNode = getFirstTextNode(endNode);
+            endOffset = endNode.textContent.length;
+        } else {
+            endNode = endNode.childNodes[endOffset];
+            endNode = getFirstTextNode(endNode);
+            endOffset = 0;
+        }
+        rangeObject.setEnd(endNode, endOffset);
+    }
 }
 
 

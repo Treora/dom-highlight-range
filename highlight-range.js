4ff4ada3fe01ec9b84523722bc2691f171505764
Gerben <gerben@treora.com>
2020-02-13 13:10:50 +0100

    Use 2-space indent instead of 4-space.

--- highlight-range.js
+++ highlight-range.js
@@ -9,119 +9,119 @@ const highlightRange = (function () {
 // - highlightElement: the element used to wrap text nodes. Defaults to 'mark'.
 // - attributes: an Object defining any attributes to be set on the wrapper elements.
 function highlightRange(rangeObject, highlightElement = 'mark', attributes = {}) {
-    if (rangeObject.collapsed) return;
+  if (rangeObject.collapsed) return;
 
-    // First put all nodes in an array (splits start and end nodes if needed)
-    const nodes = textNodesInRange(rangeObject);
+  // First put all nodes in an array (splits start and end nodes if needed)
+  const nodes = textNodesInRange(rangeObject);
 
+  // Remember range details to restore it later.
+  const startContainer = rangeObject.startContainer;
+  const startOffset = rangeObject.startOffset;
+  const endContainer = rangeObject.endContainer;
+  const endOffset = rangeObject.endOffset;
+
+  // Highlight each node
+  const highlights = [];
+  for (nodeIdx in nodes) {
+    highlights.push(highlightNode(nodes[nodeIdx], highlightElement, attributes));
+  }
+
+  // The rangeObject gets messed up by our DOM changes. Be kind and restore.
+  rangeObject.setStart(startContainer, startOffset);
+  rangeObject.setEnd(endContainer, endOffset);
+
+  // Return a function that cleans up the highlights.
+  function cleanupHighlights() {
     // Remember range details to restore it later.
     const startContainer = rangeObject.startContainer;
     const startOffset = rangeObject.startOffset;
     const endContainer = rangeObject.endContainer;
     const endOffset = rangeObject.endOffset;
 
-    // Highlight each node
-    const highlights = [];
-    for (nodeIdx in nodes) {
-        highlights.push(highlightNode(nodes[nodeIdx], highlightElement, attributes));
+    // Remove each of the created highlights.
+    for (const highlightIdx in highlights) {
+      removeHighlight(highlights[highlightIdx]);
     }
 
-    // The rangeObject gets messed up by our DOM changes. Be kind and restore.
+    // Be kind and restore the rangeObject again.
     rangeObject.setStart(startContainer, startOffset);
     rangeObject.setEnd(endContainer, endOffset);
-
-    // Return a function that cleans up the highlights.
-    function cleanupHighlights() {
-        // Remember range details to restore it later.
-        const startContainer = rangeObject.startContainer;
-        const startOffset = rangeObject.startOffset;
-        const endContainer = rangeObject.endContainer;
-        const endOffset = rangeObject.endOffset;
-
-        // Remove each of the created highlights.
-        for (const highlightIdx in highlights) {
-            removeHighlight(highlights[highlightIdx]);
-        }
-
-        // Be kind and restore the rangeObject again.
-        rangeObject.setStart(startContainer, startOffset);
-        rangeObject.setEnd(endContainer, endOffset);
-    }
-    return cleanupHighlights;
+  }
+  return cleanupHighlights;
 }
 
 // Return an array of the text nodes in the range. Split the start and end nodes if required.
 function textNodesInRange(range) {
-    // If the start or end node is a text node and only partly in the range, split it.
-    if (range.startContainer.nodeType === Node.TEXT_NODE && range.startOffset > 0) {
-        const endOffset = range.endOffset; // (this may get lost when the splitting the node)
-        const createdNode = range.startContainer.splitText(range.startOffset);
-        if (range.endContainer === range.startContainer) {
-            // If the end was in the same container, it will now be in the newly created node.
-            range.setEnd(createdNode, endOffset - range.startOffset);
-        }
-        range.setStart(createdNode, 0);
+  // If the start or end node is a text node and only partly in the range, split it.
+  if (range.startContainer.nodeType === Node.TEXT_NODE && range.startOffset > 0) {
+    const endOffset = range.endOffset; // (this may get lost when the splitting the node)
+    const createdNode = range.startContainer.splitText(range.startOffset);
+    if (range.endContainer === range.startContainer) {
+      // If the end was in the same container, it will now be in the newly created node.
+      range.setEnd(createdNode, endOffset - range.startOffset);
     }
-    if (
-        range.endContainer.nodeType === Node.TEXT_NODE
-        && range.endOffset < range.endContainer.length
-    ) {
-        range.endContainer.splitText(range.endOffset);
-    }
+    range.setStart(createdNode, 0);
+  }
+  if (
+    range.endContainer.nodeType === Node.TEXT_NODE
+    && range.endOffset < range.endContainer.length
+  ) {
+    range.endContainer.splitText(range.endOffset);
+  }
 
-    // Collect the text nodes.
-    const walker = range.startContainer.ownerDocument.createTreeWalker(
-        range.commonAncestorContainer,
-        NodeFilter.SHOW_TEXT,
-        node => range.intersectsNode(node) ? NodeFilter.FILTER_ACCEPT : NodeFilter.FILTER_REJECT,
-    );
-    walker.currentNode = range.startContainer;
+  // Collect the text nodes.
+  const walker = range.startContainer.ownerDocument.createTreeWalker(
+    range.commonAncestorContainer,
+    NodeFilter.SHOW_TEXT,
+    node => range.intersectsNode(node) ? NodeFilter.FILTER_ACCEPT : NodeFilter.FILTER_REJECT,
+  );
+  walker.currentNode = range.startContainer;
 
-    // // Optimise by skipping nodes that are explicitly outside the range.
-    // const NodeTypesWithCharacterOffset = [
-    //    Node.TEXT_NODE,
-    //    Node.PROCESSING_INSTRUCTION_NODE,
-    //    Node.COMMENT_NODE,
-    // ];
-    // if (!NodeTypesWithCharacterOffset.includes(range.startContainer.nodeType)) {
-    //     if (range.startOffset < range.startContainer.childNodes.length) {
-    //         walker.currentNode = range.startContainer.childNodes[range.startOffset];
-    //     } else {
-    //         walker.nextSibling(); // TODO verify this is correct.
-    //     }
-    // }
+  // // Optimise by skipping nodes that are explicitly outside the range.
+  // const NodeTypesWithCharacterOffset = [
+  //  Node.TEXT_NODE,
+  //  Node.PROCESSING_INSTRUCTION_NODE,
+  //  Node.COMMENT_NODE,
+  // ];
+  // if (!NodeTypesWithCharacterOffset.includes(range.startContainer.nodeType)) {
+  //   if (range.startOffset < range.startContainer.childNodes.length) {
+  //     walker.currentNode = range.startContainer.childNodes[range.startOffset];
+  //   } else {
+  //     walker.nextSibling(); // TODO verify this is correct.
+  //   }
+  // }
 
-    const nodes = [];
-    if (walker.currentNode.nodeType === Node.TEXT_NODE)
-        nodes.push(walker.currentNode);
-    while (walker.nextNode() && range.comparePoint(walker.currentNode, 0) !== 1)
-        nodes.push(walker.currentNode);
-    return nodes;
+  const nodes = [];
+  if (walker.currentNode.nodeType === Node.TEXT_NODE)
+    nodes.push(walker.currentNode);
+  while (walker.nextNode() && range.comparePoint(walker.currentNode, 0) !== 1)
+    nodes.push(walker.currentNode);
+  return nodes;
 }
 
 // Replace [node] with <highlightElement ...attributes>[node]</highlightElement>
 function highlightNode(node, highlightElement, attributes) {
-    const highlight = document.createElement(highlightElement);
-    Object.keys(attributes).forEach(key => {
-        highlight.setAttribute(key, attributes[key]);
-    });
-    node.parentNode.replaceChild(highlight, node);
-    highlight.appendChild(node);
-    return highlight;
+  const highlight = document.createElement(highlightElement);
+  Object.keys(attributes).forEach(key => {
+    highlight.setAttribute(key, attributes[key]);
+  });
+  node.parentNode.replaceChild(highlight, node);
+  highlight.appendChild(node);
+  return highlight;
 }
 
 // Remove a highlight element created with highlightNode.
 function removeHighlight(highlight) {
-    // Move its children (normally just one text node) into its parent.
-    while (highlight.firstChild) {
-        highlight.parentNode.insertBefore(highlight.firstChild, highlight);
-    }
-    highlight.remove();
+  // Move its children (normally just one text node) into its parent.
+  while (highlight.firstChild) {
+    highlight.parentNode.insertBefore(highlight.firstChild, highlight);
+  }
+  highlight.remove();
 }
 
 return highlightRange;
 })();
 
 if (typeof module !== 'undefined') {
-    module.exports = highlightRange;
+  module.exports = highlightRange;
 }
